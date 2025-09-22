43c564e91545165fff5bad6943348cd0eb376258
jrodriguezcancio <jrodriguezcancio@udesa.edu.ar>
2025-09-22 16:29:29 -0300

    nombre usuario

--- routes/search-results.js
+++ routes/search-results.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const searchResultsController = require("../controllers/search-resultsController");
 const productController = require('../controllers/productController');
 
 router.get('/', productController.searchResults)
