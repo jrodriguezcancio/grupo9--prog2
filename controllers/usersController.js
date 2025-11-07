db8a391a3a47fd572dc4e97d6f18d0fb43a48a9f
thiagomarucco <tmarucco@udesa.edu.ar>
2025-11-06 22:29:06 -0300

    avance con estructura de controladores y correcciones generales

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,17 +2,28 @@ const olddb = require('../localData/db');
 let db = require("../database/models");
 
 const usersController = {
-    register: function(req, res){
-        res.render('register', { allproducts: db.productos, usuario: false });
+    register: function(req, res){ //solo debe aparecer si no esta loagueado con un if, se enacrga de renderizar nada mas
+        res.render('register');
     },
-    login : function (req, res) {
-        const busqueda = req.query.search;
-        return res.render("login", { termino: busqueda, allproducts: db.productos });
+    createRegister: function(req, res){ //encargado de procesar el registro y cargarlo en la db, POST
+        res.render('register');
     },
-    profile: function (req, res) {
+    profile: function (req, res) { // 
         const usuario = db.usuario;
-        res.render('profile', { usuario:true , allproducts: db.productos});
+        res.render('profile');
     },
+    login : function (req, res) { //solo debe aparecer si no esta loagueado con un if, se enacrga de renderizar nada mas
+        return res.render("login");
+    },
+    processLogin: function (req, res) {
+        res.render("/") //falta todo session y cookies para acceder a la db y ver si el form coincide con la db, POST
+    },
+    logout: function (req, res) { //todavia no va a funcionar , hay que procesar el login para hacer un logout
+        req.session.destroy();
+        res.clearCookie('userData');
+
+        return res.redirect("/");
+    }
 
 }
 
--- routes/product.js
+++ routes/product.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const productController = require('../controllers/productController')
 
 router.get('/search-results', productController.searchResults);
-router.get('/:id?', productController.product);
 router.get('/product-add', productController.productAdd);
+router.get('/:id?', productController.product);
 
 
 module.exports = router;
